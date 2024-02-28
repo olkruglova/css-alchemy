8d128e795023f471e758d6413eb4c42bf8b7ecce
Olga Kruglova <olha.kruhlova@inetum.com>
2024-02-28 12:34:03 +0100

    #added nav-menu

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -8,21 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   public tabItems: any | null = null;
 
-  public activeItem: any | null = null;
-
   ngOnInit() {
     this.tabItems = [
-      { label: 'Main', routerLink: 'main' },
-      { label: 'Articles', routerLink: 'main1' },
-      { label: 'Edit', routerLink: 'main2' },
-      { label: 'Documentation', routerLink: 'main3' },
-      { label: 'Settings', routerLink: 'main4' },
+      { label: 'Main', routerLink: '/main', name: 'main' },
+      { label: 'Articles', routerLink: '/articles', name: 'articles' },
+      { label: 'Edit', routerLink: '/main2', name: 'main' },
+      { label: 'Documentation', routerLink: '/main3', name: 'main' },
+      { label: 'About', routerLink: '/about', name: 'about' },
     ];
-
-    this.activeItem = this.tabItems[0];
-  }
-
-  public onActiveItemChange(event: any) {
-    this.activeItem = event;
   }
 }
--- src/app/features/header/header.module.ts
+++ src/app/features/header/header.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [RouterModule],
+  imports: [SharedModule],
   exports: [HeaderComponent],
   providers: [],
   bootstrap: [HeaderComponent],
